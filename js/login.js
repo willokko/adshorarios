6ca95f2408f8aa5e4ebbc49ed9a51fda9ed665c2
José Wilson <jwilsonpereira720@gmail.com>
2025-03-06 14:08:40 -0300

    feat: register and login

--- js/login.js
+++ js/login.js
@@ -1,12 +1,18 @@
-function logar(){
-    var login = document.getElementById('login').value;
-    var senha = document.getElementById('senha').value;
-    
-    if(login == "adm@gmail.com" && senha == "adm"){
-        alert('Login efetuado com sucesso!');
+function logar() {
+    const login = document.getElementById('login').value;
+    const senha = document.getElementById('senha').value;
+
+    // Carregar usuários existentes do localStorage
+    const usuarios = JSON.parse(localStorage.getItem('usuarios')) || [];
+
+    // Verificar se o usuário existe
+    const usuario = usuarios.find(usuario => usuario.email === login);
+
+    if (!usuario) {
+        alert('Usuário não existe, se registre primeiro.');
+    } else if (usuario.senha !== senha) {
+        alert('Senha errada, digite a correta.');
+    } else {
         location.href = "index.html"; 
-    }
-    else{
-        alert('Algo está errado. Tente novamente.');
     }
 }
