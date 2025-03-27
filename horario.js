9c284860015822069bb65e0a520bf3e926afc09f
José Wilson <jwilsonpereira720@gmail.com>
2025-03-27 12:28:55 -0300

    fix: Horários atualizados

--- horario.js
+++ horario.js
@@ -1,51 +1,52 @@
 const periodos = {
     "1º Período": [
-        { nome: "Cálculo", horarios: ["M1Seg", "M2Seg", "M3Ter","M1Qua", "M2Qua", "M3Qua"] },
-        { nome: "Algoritmos", horarios: ["M1Ter", "M2Ter", "M4Qui", "M5Qui", "M3Sex", "M4Sex", "M5Sex"] },
-        { nome: "Fundamentos de Computação", horarios: ["M3Seg", "M4Seg", "M4Ter", "M5Ter"] },
-        { nome: "Linguagem de Marcação", horarios: ["M1Qui", "M2Qui", "M1Sex", "M2Sex"] },
-        { nome: "Inglês Instrumental", horarios: ["M5Seg", "M3Qui"] },
-        { nome: "Português", horarios: ["M4Qua", "M5Qua"] },
+        { nome: "Matemática Aplicada a Computação", horarios: ["M1Qua", "M2Qua", "M4Qui", "M5Qui"] },
+        { nome: "Algoritmos", horarios: ["M1Ter", "M2Ter", "M1Qui", "M2Qui", "M3Qui", "M5Sex"] },
+        { nome: "Fundamentos de Computação", horarios: ["M3Ter", "M3Qua", "M4Qua","M5Qua"] },
+        { nome: "Linguagem de Marcação", horarios: ["M1Sex", "M2Sex", "M3Sex", "M4Sex"] },
+        { nome: "Inglês Instrumental", horarios: ["M4Ter", "M5Ter"] },
+        { nome: "Português", horarios: ["M4Seg", "M5Seg"] },
+        { nome: "Relações Humanas no Trabalho", horarios: ["M1Seg", "M2Seg", "M3Seg"] },
     ],
     "2º Período": [
         { nome: "Gerenciamento de Mudanças", horarios: ["T1Seg", "T2Seg"] },
-        { nome: "Probabilidade", horarios: ["T1Ter", "T2Ter", "T1Qua", "T2Qua", "T3Qua"] },
+        { nome: "Probabilidade", horarios: ["T1Ter", "T2Ter", "T1Qua", "T2Qua"] },
         { nome: "Estruturas de Dados", horarios: ["T3Seg", "T4Seg", "T5Seg", "T3Ter", "T4Ter", "T5Ter"] },
-        { nome: "Redes", horarios: ["T4Qua", "T5Qua", "T1Qui", "T2Qui", "T3Qui", "T4Qui"] },
-        { nome: "Laboratório de Sistemas Web", horarios: ["T5Qui","T1Sex ", "T2Sex", "T3Sex"] },
-        { nome: "Sociedade e Tecnologia da Informação", horarios: ["T4Sex", "T5Sex"] },
+        { nome: "Inclusão Tecnológica", horarios: ["T3Qua","T4Qua", "T5Qua"] },
+        { nome: "Linguagens de Script para Web", horarios: ["T1Qui", "T2Qui", "T1Sex ", "T2Sex"] },
+        { nome: "Sistemas Operacionais", horarios: ["T5Qui" ,"T3Sex" ,"T4Sex", "T5Sex"] },
+        { nome: "Metodologia", horarios: ["T3Qui", "T4Qui"] },
     ],
     "3º Período": [
-        { nome: "Projeto e Desenvolvimento de Software", horarios: ["M1Seg", "M2Seg", "M1Ter", "M2Ter", "M3Ter"] },
-        { nome: "Programação Orientada a Objetos", horarios: ["M3Seg", "M4Seg", "M5Seg", "M1Qua", "M2Qua", "M3Qua"] },
-        { nome: "Banco de Dados I", horarios: ["M4Ter", "M5Ter", "M1Qui", "M2Qui", "M3Qui", "M1Sex", "M2Sex"] },
-        { nome: "Metodologia", horarios: ["M4Qua", "M5Qua"] },
-        { nome: "Sistemas Operacionais", horarios: ["M4Qui", "M5Qui","M3Sex", "M4Sex", "M5Sex"] },
+        { nome: "Engenharia de Software", horarios: ["M4Seg", "M5Seg", "M1Ter", "M2Ter", "M3Ter"] },
+        { nome: "Programação Orientada a Objetos", horarios: ["M1Seg", "M2Seg", "M3Seg", "M1Qua", "M2Qua", "M3Qua"] },
+        { nome: "Banco de Dados I", horarios: ["M4Ter", "M5Ter", "M4Qui", "M5Qui", "M1Sex", "M2Sex"] },
+        { nome: "Direitos Sociais e Humanos", horarios: ["M4Qua", "M5Qua"] },
+        { nome: "Redes", horarios: ["M1Qui", "M2Qui", "M3Qui", "M3Sex", "M4Sex", "M5Sex"] },
     ],
     "4º Período": [
-        { nome: "Análise e Projeção de Sistemas", horarios: ["T1Seg", "T2Seg", "T3Seg", "T4Ter", "T5Ter"] },
-        { nome: "Laboratório de Redes", horarios: ["T1Ter", "T2Ter", "T3Ter"] },
-        { nome: "Banco de Dados II", horarios: ["T4Seg", "T5Seg", "T1Qua", "T2Qua", "T3Qua"] },
+        { nome: "Laboratório de Engenharia de Software", horarios: ["T1Seg", "T2Seg", "T3Seg", "T1Ter", "T2Ter"] },
+        { nome: "Laboratório de Redes", horarios: ["T1Qua", "T2Qua", "T3Qua"] },
+        { nome: "Banco de Dados II", horarios: ["T4Seg", "T5Seg", "T3Ter", "T4Ter", "T5Ter"] },
         { nome: "Teste de Software", horarios: ["T1Qui", "T2Qui", "T3Qui"] },
         { nome: "Gerenciamento de Projetos", horarios: ["T4Qua", "T5Qua", "T1Sex", "T2Sex"] },
         { nome: "Programação Web I", horarios: ["T4Qui", "T5Qui", "T3Sex", "T4Sex", "T5Sex"] },
     ],
     "5º Período": [
-        { nome: "Interação Humano-Computador", horarios: ["M1Seg", "M2Seg", "M1Ter", "M2Ter"] },
-        { nome: "Desenvolvimento de Aplicações Corporativas", horarios: ["M3Seg", "M4Seg", "M5Seg", "M1Qua", "M2Qua", "M3Qua"] },
-        { nome: "Segurança da Informação", horarios: ["M3Ter", "M4Ter", "M5Ter", "M1Qui", "M2Qui"] },
-        { nome: "Padrões de Projeto", horarios: ["M4Qua", "M5Qua", "M3Qui","M4Qui", "M5Qui"] },
-        { nome: "Programação para Dispositivos Móveis", horarios: ["M2Sex", "M3Sex", "M4Sex", "M5Sex"] },
-        { nome: "Trabalho de Conclusão de Curso I", horarios: ["T4Qua", "T5Qua"] },
+        { nome: "Empreendedorismo", horarios: ["M4Seg", "M5Seg", "T2Qui"] },
+        { nome: "Arquitetura e Padrões de Projeto de Software", horarios: ["M1Ter", "M2Ter", "M1Qua", "M2Qua", "M3Qua"] },
+        { nome: "Segurança de Dados", horarios: ["M4Qui", "M5Qui", "M1Sex", "M2Sex"] },
+        { nome: "Programação Web II", horarios: ["M3Ter", "M4Ter", "M5Ter", "M4Qua", "M5Qua"] },
+        { nome: "Práticas Curriculares em Sociedade I", horarios: ["M1Seg", "M2Seg", "M3Seg", "M1Qui", "M2Qui", "M3Qui"] },
+        { nome: "Gestão de Tecnologia da Informação", horarios: ["M4Sex", "M5Sex"] },
     ],
     "6º Período": [
-        { nome: "Trabalho de Conclusão de Curso II", horarios: ["M1Ter", "M2Ter"] },
-        { nome: "Língua Brasileira de Sinais", horarios: ["M3Ter", "M4Ter", "M5Ter"] },
-        { nome: "Empreendedorismo", horarios: ["T1Seg", "T2Seg", "T3Seg", "T1Ter"] },
-        { nome: "Recursos Humanos em TI", horarios: ["T2Ter", "T3Ter", "T4Qua", "T5Qua"] },
-        { nome: "Sistemas Distribuídos", horarios: ["T4Ter", "T5Ter", "T1Qua", "T2Qua", "T3Qua"] },
-        { nome: "Programação Web II", horarios: ["T1Qui","T2Qui", "T3Qui", "T1Sex", "T2Sex"] },
-        { nome: "Gestão de Tecnologia da Informação", horarios: ["T4Seg", "T5Seg", "T4Qui", "T5Qui"] },
+        { nome: "História na Informática", horarios: ["M1Seg", "M2Seg", "M5Qui"] },
+        { nome: "Interação Humano-Computador", horarios: ["M3Ter", "M4Ter", "M5Ter", "M5Sex"] },
+        { nome: "Desenvolvimento de Aplicações Corporativas", horarios: ["M3Seg", "M4Seg", "M5Seg", "M1Ter", "M2Ter"] },
+        { nome: "Práticas Curriculares em Sociedade II", horarios: ["M1Qua", "M2Qua", "M3Qua", "M4Qua", "M5Qua", "M4Sex"] },
+        { nome: "Sistemas Distribuídos", horarios: ["M1Sex", "M2Sex", "M3Sex"] },
+        { nome: "Programação para Dispositivos Móveis", horarios: ["M1Qui", "M2Qui", "M3Qui", "M4Qui"] },
     ]
 };
 
@@ -139,12 +140,9 @@ function gerarHorario() {
     }
     document.querySelectorAll('input[type="checkbox"]:checked').forEach(checkbox => {
         const materiaItem = checkbox.closest('.materia-item');
-        // Obtém nome da matéria
         const materiaName = materiaItem.querySelector('label').innerText;
-        // Obtém array de horários (exemplo: ["M1Seg", "M2Seg"])
         const horariosTexto = checkbox.value.split(', '); 
         horariosTexto.forEach(codigo => {
-            // Encontra a célula correspondente pelo ID
             const cell = tabelaHorarios.querySelector(`#${codigo}`);
             if (cell) {
                 if (cell.innerHTML) {
